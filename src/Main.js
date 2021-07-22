d409ee5f4cf9cd761c2a19c9f4f3c3f713da8f9a
satkar2001 <satkaracharya883@gmail.com>
2021-07-22 21:06:22 +0530

    css style added

--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,40 @@
 import React,{useState,useEffect} from 'react'
 import axios from 'axios'
+import 'jquery/dist/jquery.min.js';
+import 'datatables.net-dt/js/dataTables.dataTables'
+import 'datatables.net-dt/css/jquery.dataTables.min.css'
+import $ from 'jquery';
+
 export default function Main() {
+        const[coviddata,setcoviddata]=useState([])
+
     useEffect(() => {
          axios.get('https://api.covid19api.com/summary').then(res=>{
              console.log(res)
+             setcoviddata(res.data.Countries)
          }).catch(err=>{
              console.log(err)
+
+         })
+         $(document).ready(()=>{
+             $('#mytable').DataTable()
          })
     }, [])
-
+        const tabledata=coviddata.map(obj=>{
+            return <tr>
+                <td>{obj.Country}</td>
+                <td>{obj.TotalConfirmed}</td>
+                <td>{obj.TotalConfirmed - obj.TotalRecovered}</td>
+                <td>{obj.TotalRecovered}</td>
+                <td>{obj.TotalDeaths}</td>
+            </tr>
+        })
     return (
         <div>
-            <h1>COVID-STATS</h1>
+            <h1 style={{backgroundColor:'black' ,color:'white'}}>COVID-STATS</h1>
             <div className="row justify-content-center">
                 <div className="col-md-8">
-                    <table className='table table-dark'>
+                    <table id='mytable' className='table table-bordered'>
                         <thead>
                             <tr>
                             <th>country</th>
@@ -24,7 +44,9 @@ export default function Main() {
                             <th>deaths</th>
                             </tr>
                         </thead>
-
+                         <tbody>
+                             {tabledata}
+                         </tbody>
                     </table>
                 </div>
             </div>
